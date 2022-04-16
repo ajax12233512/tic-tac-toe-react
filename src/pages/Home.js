f2124fe8820b6103a00ea3caeef3954eba50f0b6
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-16 10:28:42 -0500

    added desktop view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/HomeComponents/Header'
 import Main from '../components/HomeComponents/Main'
+import './Home.css'
 function Home() {
   return (
     <div id='home'>
